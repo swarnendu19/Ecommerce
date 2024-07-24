28cd5a23f84cd0b7a3c7e66212ed5e7a14960778
Swarnendu <swarnendumaity633@gmail.com>
2024-07-24 12:03:36 +0530

    FIxing Bugs on Deployment

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -1,16 +1,14 @@
-module.exports = {
-    apps: [
-        {
-            name: "ecommerce-app",
-            script: "./dist/index.js",
-            instances: "max",
-            exec_mode: "cluster",
-            env: {
-                NODE_ENV: "development",
-            },
-            env_production: {
-                NODE_ENV: "production",
-            },
+export const apps = [
+    {
+        name: "ecommerce-app",
+        script: "./dist/index.js",
+        instances: "max",
+        exec_mode: "cluster",
+        env: {
+            NODE_ENV: "development",
         },
-    ],
-};
+        env_production: {
+            NODE_ENV: "production",
+        },
+    },
+];
